796f4dbba4c56b13aaa14ebcdb131ddeed6e2ce9
Kamlesh Pokiya <kamleshpokiya1993@gmail.com>
2024-08-23 11:20:52 +0530

    fix edit page ui

--- src/app/(pages)/createmovie/[id]/page.tsx
+++ src/app/(pages)/createmovie/[id]/page.tsx
@@ -5,12 +5,11 @@ import React from "react";
 const page = ({ params }: { params: { id: string } }) => {
   return (
     <div className="max-w-[1440px] px-[16px] py-[60px] xl:px-[120px] xl:py-[120px] mx-auto">
-          <h1 className="text-white font-semibold text-[32px] lg:text-[48px]">
-            {params.id == "Edit" ? "Edit movie" : "Create a new movie"}
-          </h1>
+      <h1 className="text-white font-semibold text-[32px] lg:text-[48px]">
+        {params.id == "Edit" ? "Edit movie" : "Create a new movie"}
+      </h1>
       <div className="md:grid grid-cols-12 gap-6">
         <div className="col-span-6">
-
           <div className="mt-20 lg:mt-[120px] space-y-6">
             <div className="md:hidden flex flex-col gap-6">
               <input
