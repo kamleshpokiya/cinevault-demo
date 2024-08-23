5b188c886f885d8e4132d57f89b274230321952d
Kamlesh Pokiya <kamleshpokiya1993@gmail.com>
2024-08-23 11:57:55 +0530

    ui changes

--- src/app/(pages)/createmovie/[id]/page.tsx
+++ src/app/(pages)/createmovie/[id]/page.tsx
@@ -4,13 +4,12 @@ import React from "react";
 
 const page = ({ params }: { params: { id: string } }) => {
   return (
-    <div className="max-w-[1440px] px-[16px] py-[60px] xl:px-[120px] xl:py-[120px] mx-auto">
-          <h1 className="text-white font-semibold text-[32px] lg:text-[48px]">
-            {params.id == "Edit" ? "Edit movie" : "Create a new movie"}
-          </h1>
+    <div className="max-w-[1440px] px-[24px] py-[60px] xl:px-[120px] xl:py-[120px] mx-auto">
+      <h1 className="text-white font-semibold text-[32px] lg:text-[48px]">
+        {params.id == "Edit" ? "Edit movie" : "Create a new movie"}
+      </h1>
       <div className="md:grid grid-cols-12 gap-6">
         <div className="col-span-6">
-
           <div className="mt-20 lg:mt-[120px] space-y-6">
             <div className="md:hidden flex flex-col gap-6">
               <input
@@ -52,12 +51,12 @@ const page = ({ params }: { params: { id: string } }) => {
             <SubmitButton
               label="Cancel"
               variant="secondary"
-              classbtn="!px-[64px] sm:!px-[55px] !py-[16px] !h-[unset] !w-[unset]"
+              classbtn="!px-[60px] sm:!px-[55px] !py-[16px] !h-[unset] !w-[unset]"
             />
             <SubmitButton
               label="Submit"
               variant="primary"
-              classbtn="!px-[64px] sm:!px-[55px] !py-[16px] !h-[unset] !w-[unset]"
+              classbtn="!px-[60px] sm:!px-[55px] !py-[16px] !h-[unset] !w-[unset]"
             />
           </div>
         </div>
